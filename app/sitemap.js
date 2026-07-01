a5b7362f0413d34f76674e88c2dd346752997d1a
rajeev522 <rajeevgupta09871@gmail.com>
2026-07-01 23:27:47 +0530

    Add sitemap function for site URLs

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://ballia-institute.vercel.app",
+      lastModified: new Date(),
+      priority: 1,
+      changeFrequency: "daily",
+    },
+  ];
+}
